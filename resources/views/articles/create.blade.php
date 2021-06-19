16dc76b5aed3266e82ce0c96e569eedf04494023
Stefan Wurpel <wurp0001@hz.nl>
2021-06-19 17:51:03 +0200

    Update create.blade.php

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,19 +7,19 @@
             @csrf
 
             <label for="Title">* Title:</label><br>
-            <input type="text" id="Title" name="title" placeholder="Title" style="height: 42px; width: 50%">
+            <input type="text" id="Title" name="Title" placeholder="Title" style="height: 42px; width: 50%">
 
             @error('Title')
             <p class="error">{{$errors->first('Title')}}</p>
             @enderror
             <br><br>
             <label for="Excerpt">* Excerpt:</label><br>
-            <textarea cols="2" rows="5" type="text" id="excerpt" name="excerpt" placeholder="short version" style="height: 125px; width: 50%"></textarea>
+            <textarea cols="2" rows="5" type="text" id="Excerpt" name="Excerpt" placeholder="short version" style="height: 125px; width: 50%"></textarea>
             @error('Excerpt')
             <p class="error">{{$errors->first('Excerpt')}}</p>
             @enderror<br><br>
             <label for="Body">* Body:</label><br>
-            <textarea cols="5" rows="5" type="text" id="body" name="body" placeholder="full version" style="height: 375px; width: 50%"></textarea>
+            <textarea cols="5" rows="5" type="text" id="Body" name="Body" placeholder="full version" style="height: 375px; width: 50%"></textarea>
             @error('Body')
             <p class="error">{{$errors->first('Body')}}</p>
             @enderror<br><br>
