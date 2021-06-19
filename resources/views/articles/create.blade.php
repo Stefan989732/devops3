dfb362f60d323338dbcd3cf5a47c8ba3542c3f22
Stefan Wurpel <wurp0001@hz.nl>
2021-06-19 17:54:43 +0200

    Update create.blade.php

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -9,19 +9,19 @@
             <label for="Title">* Title:</label><br>
             <input type="text" id="Title" name="title" placeholder="Title" style="height: 42px; width: 50%">
 
-            @error('title')
-            <p class="error">{{$errors->first('title')}}</p>
+            @error('Title')
+            <p class="error">{{$errors->first('Title')}}</p>
             @enderror
             <br><br>
             <label for="excerpt">* Excerpt:</label><br>
             <textarea cols="2" rows="5" type="text" id="excerpt" name="excerpt" placeholder="short version" style="height: 125px; width: 50%"></textarea>
-            @error('excerpt')
-            <p class="error">{{$errors->first('excerpt')}}</p>
+            @error('Excerpt')
+            <p class="error">{{$errors->first('Excerpt')}}</p>
             @enderror<br><br>
             <label for="body">* Body:</label><br>
             <textarea cols="5" rows="5" type="text" id="body" name="body" placeholder="full version" style="height: 375px; width: 50%"></textarea>
-            @error('body')
-            <p class="error">{{$errors->first('body')}}</p>
+            @error('Body')
+            <p class="error">{{$errors->first('Body')}}</p>
             @enderror<br><br>
             <button type="submit" value="Submit">Submit</button>
         </form>
